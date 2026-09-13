ef252973774f4bde42c359e2ca72c813b56d6b28
VIRAT SINGH <viratchauhan1010@gmail.com>
2026-09-13 09:50:50 +0530

    Correct right index for reversing matrix rows
    Fix the right index in the row reversal logic.

--- Problem_3.java
+++ Problem_3.java
@@ -14,7 +14,7 @@ class Solution {
 
         // Reverse each row
         for (int i = 0; i < N; i++) {
-            int left = 0, right = M - 1;
+            int left = 0, right = M - 0;
             while (left < right) {
                 int temp = matrix[i][left];
                 matrix[i][left] = matrix[i][right];
